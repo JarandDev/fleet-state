f6bbf4f7d309c3a9395d22542d73b72c17ffbdc3
JarandDev <jarand95@gmail.com>
2023-03-11 09:52:06 +0100

    Fix imports in test

--- src/test/kotlin/dev/jarand/fleetstate/monitoring/MonitoringComponentTest.kt
+++ src/test/kotlin/dev/jarand/fleetstate/monitoring/MonitoringComponentTest.kt
@@ -2,17 +2,17 @@ package dev.jarand.fleetstate.monitoring
 
 import dev.jarand.fleetstate.config.ComponentTest
 import org.junit.jupiter.api.Test
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
 class MonitoringComponentTest : ComponentTest() {
 
     @Test
     fun `GET health should return 200 and expected json`() {
-        mockMvc.perform(MockMvcRequestBuilders.get("/actuator/health"))
-            .andExpect(MockMvcResultMatchers.status().isOk)
+        mockMvc.perform(get("/actuator/health"))
+            .andExpect(status().isOk)
             .andExpect(
-                MockMvcResultMatchers.content().json(
+                content().json(
                     """
                     {
                       "status": "UP",
@@ -28,10 +28,10 @@ class MonitoringComponentTest : ComponentTest() {
 
     @Test
     fun `GET health liveness group should return 200 and expected json`() {
-        mockMvc.perform(MockMvcRequestBuilders.get("/actuator/health/liveness"))
-            .andExpect(MockMvcResultMatchers.status().isOk)
+        mockMvc.perform(get("/actuator/health/liveness"))
+            .andExpect(status().isOk)
             .andExpect(
-                MockMvcResultMatchers.content().json(
+                content().json(
                     """
                     {
                       "status": "UP"
@@ -43,10 +43,10 @@ class MonitoringComponentTest : ComponentTest() {
 
     @Test
     fun `GET health readiness group should return 200 and expected json`() {
-        mockMvc.perform(MockMvcRequestBuilders.get("/actuator/health/readiness"))
-            .andExpect(MockMvcResultMatchers.status().isOk)
+        mockMvc.perform(get("/actuator/health/readiness"))
+            .andExpect(status().isOk)
             .andExpect(
-                MockMvcResultMatchers.content().json(
+                content().json(
                     """
                     {
                       "status": "UP"
@@ -58,9 +58,9 @@ class MonitoringComponentTest : ComponentTest() {
 
     @Test
     fun `GET prometheus should return 200 and expected content type`() {
-        mockMvc.perform(MockMvcRequestBuilders.get("/actuator/prometheus"))
-            .andExpect(MockMvcResultMatchers.status().isOk)
-            .andExpect(MockMvcResultMatchers.header().string("Content-Type", "text/plain;version=0.0.4;charset=utf-8"))
+        mockMvc.perform(get("/actuator/prometheus"))
+            .andExpect(status().isOk)
+            .andExpect(header().string("Content-Type", "text/plain;version=0.0.4;charset=utf-8"))
 
     }
 }
